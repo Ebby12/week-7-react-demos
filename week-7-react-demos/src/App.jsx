351b33d2cd0e66e4368b0421114f6410079e51c2
Kat Law <kathleen.law@multiverse.io>
2023-05-16 12:37:36 -0400

    add state (#2)

--- week-7-react-demos/src/App.jsx
+++ week-7-react-demos/src/App.jsx
@@ -1,24 +1,30 @@
+import { useState } from "react";
 import ListItem from "./components/ListItem";
 
 function App() {
-
+  const [isTaskListVisible, setIsTaskListVisible] = useState(false);
   const data = [{
     title: "have lunch",
     description: "lunch",
     time: "now"
-  }, {
+  }, 
+  {
     title: "Run Learning Squads",
     description: "amazing stuff",
     time: "2pm ET"
-  }, {
+  }, 
+  {
     title: "Complete exit tickets",
     description: "surveys and stuff",
     time: "5pm ET"
   }];
 
+  const handleClick = () => setIsTaskListVisible(!isTaskListVisible)
+  // condition that returns a boolean ? what to run if condition is true : what to run if condition is false
   return (
     <div>
-      <ol>
+      <button onClick={handleClick}>{isTaskListVisible ? 'Hide Tasks' : 'Show Tasks'}</button>
+      {isTaskListVisible && (<ol>
         {/* <ListItem taskName="have lunch" description="lunch" time="1pm" />
         <ListItem taskName="start work" description="make react demo" time="9am" /> */}
         {
@@ -31,7 +37,7 @@ function App() {
             />
            ) )
         }
-      </ol>
+      </ol>)}
     </div>
   )
 }

b2c63acbdb056563c7895a5728c4d6f013090cdd
Kat Law <kathleen.law@multiverse.io>
2023-05-17 13:25:48 -0400

    form (#3)

--- week-7-react-demos/src/App.jsx
+++ week-7-react-demos/src/App.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
+import Form from "./components/Form";
 import ListItem from "./components/ListItem";
 
 function App() {
   const [isTaskListVisible, setIsTaskListVisible] = useState(false);
-  const data = [{
+  const [data, setData] = useState([{
     title: "have lunch",
     description: "lunch",
     time: "now"
@@ -17,28 +18,34 @@ function App() {
     title: "Complete exit tickets",
     description: "surveys and stuff",
     time: "5pm ET"
-  }];
+  }]);
 
   const handleClick = () => setIsTaskListVisible(!isTaskListVisible)
   // condition that returns a boolean ? what to run if condition is true : what to run if condition is false
   return (
-    <div>
-      <button onClick={handleClick}>{isTaskListVisible ? 'Hide Tasks' : 'Show Tasks'}</button>
-      {isTaskListVisible && (<ol>
-        {/* <ListItem taskName="have lunch" description="lunch" time="1pm" />
-        <ListItem taskName="start work" description="make react demo" time="9am" /> */}
-        {
-          data.map( (task) => (
-            // which component do I want to display?
-            <ListItem 
-              taskName={task.title}
-              description={task.description}
-              time={task.time}
-            />
-           ) )
-        }
-      </ol>)}
-    </div>
+    <>
+      <div>
+        <Form data={data} setData={setData}/>
+      </div>
+      <div>
+        <button onClick={handleClick}>{isTaskListVisible ? 'Hide Tasks' : 'Show Tasks'}</button>
+        {isTaskListVisible && (<ol>
+          {/* <ListItem taskName="have lunch" description="lunch" time="1pm" />
+          <ListItem taskName="start work" description="make react demo" time="9am" /> */}
+          {
+            data.map( (task, index) => (
+              // which component do I want to display?
+              <ListItem 
+                key={index}
+                taskName={task.title}
+                description={task.description}
+                time={task.time}
+              />
+            ) )
+          }
+        </ol>)}
+      </div>
+    </>
   )
 }
 
